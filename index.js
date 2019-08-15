7fa714c03c35cdb6b6d140a5bed6f326f7268109
leroy <leroy.liu@ximalaya.com>
2019-08-15 18:30:07 +0800

    登录验证

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const routes = require('./routes/api');
 const mongoose = require('mongoose');
 
+var expressJwt = require('express-jwt');
 
 const app = express();
 
 // 连接数据库
 mongoose.connect('mongodb://localhost:27017/myBlog');
 mongoose.Promise = global.Promise;
+
+// jwt中间件
+app.use(
+  expressJwt({
+    secret: 'secret' //加密密钥，可换
+  }).unless({
+    path: [
+      { url: '/login', methods: ['POST'] },
+      { url: '/blog', methods: ['GET'] },
+      { url: /^\/blog\/.*$/, methods: ['GET'] },
+      '/login',
+      
+    ] 
+  })
+);
 
 app.use(express.json());
 
